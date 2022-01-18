cbfc0cae84a3b1c1b2188e2a5bed8a52ae5386f2
RYO <xenogearmaria@yahoo.co.jp>
2022-01-19 02:14:50 +0900

    コントローラー１

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks/show'
+  get 'tasks/new'
+  get 'tasks/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
